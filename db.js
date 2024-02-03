a85b4e4712f9e2f0c591aa27cacb9dde88d97351
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-04 02:29:14 +0600

    db config updated

--- db.js
+++ db.js
@@ -1,16 +1,21 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("prep-meet-db", "postgres", "5t65", {
-  host: "localhost",
-  port: 5432,
-  dialect: "postgres",
-  createdAt: "createdAt",
-  updatedAt: "updatedAt",
-  models: [__dirname + "/models/*.js"],
-  // logging: (msg) => {
-  //   // console.log(sequelize.config);
-  //   console.log("Message: ", msg);
-  // },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres",
+    createdAt: "createdAt",
+    updatedAt: "updatedAt",
+    models: [__dirname + "/models/*.js"],
+    // logging: (msg) => {
+    //   // console.log(sequelize.config);
+    //   console.log("Message: ", msg);
+    // },
+  }
+);
 
 module.exports = sequelize;
