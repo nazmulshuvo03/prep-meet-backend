7fa56c0ed4ba4f1988408b6ca924a490956774db
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-05 20:53:20 +0600

    cors origin updated

--- app.js
+++ app.js
@@ -31,8 +31,25 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: process.env.ALLOWED_ORIGINS.split(","),
-    credentials: true,
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      console.log(origin);
+      if (!origin) {
+        console.log("no origin");
+        return callback(null, true);
+      }
+      const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+      if (allowedOrigins.indexOf(origin) === -1) {
+        console.log("cors issue", origin);
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin." +
+          origin;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 app.use(bodyParser.json());
