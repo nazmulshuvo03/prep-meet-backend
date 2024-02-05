c2fdb3ed61d834196d4c4607b6dc1b017723bfae
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-05 20:57:02 +0600

    cors origin fixed

--- app.js
+++ app.js
@@ -32,8 +32,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
     origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
       console.log(origin);
       if (!origin) {
         console.log("no origin");
