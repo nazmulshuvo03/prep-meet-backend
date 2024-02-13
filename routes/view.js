95e3c5de278328350a79f85a9b341b92be04f0cf
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-13 23:27:54 +0600

    title updated

--- routes/view.js
+++ routes/view.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Prep Meet" });
+  res.render("index", { title: "Candidace" });
 });
 
 module.exports = router;
