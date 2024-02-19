0b99045abe1216189507d3885ca52338d4fad8ca
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-20 00:14:52 +0600

    require auth updated

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,15 @@ const { requireAuth } = require("../middlewares/auth");
 const router = Router();
 
 router.route("/").get((req, res) => {
-  res.json({ message: "Hello from the Prep Meet backend!!!" });
+  res.json({
+    message:
+      "Hello from the Prep Meet backend. Last updated 20/02/2024 12:14 AM",
+  });
 });
 
 router.use("/auth", authRoutes);
 router.use("/file", fileRoutes);
-// router.use(requireAuth);
+router.use(requireAuth);
 router.use("/user", userRoutes);
 router.use("/profession", professionRoutes);
 router.use("/availability", availabilityRoutes);
