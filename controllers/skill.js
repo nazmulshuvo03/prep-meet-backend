a250de1fe712779c68e5ac668ff604b83eb64125
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-26 15:15:17 +0600

    experience types controlers update

--- controllers/skill.js
+++ controllers/skill.js
@@ -1,9 +1,8 @@
 const { Op } = require("sequelize");
 const asyncWrapper = require("../middlewares/async");
 const { BAD_REQUEST } = require("../constants/errorCodes");
-const { Skill, ExperienceType } = require("../models/skill");
+const { Skill } = require("../models/skill");
 
-/********************* Skills APIs *******************************/
 const _getSkillsOfProfession = async (profession_id) => {
   const data = await Skill.findAll({ where: { profession_id } });
   return data;
@@ -51,67 +50,10 @@ const deleteSkill = asyncWrapper(async (req, res) => {
   res.success("Skill Deleted");
 });
 
-/********************* Experience Types APIs *******************************/
-const _getExperienceTypesOfProfession = async (profession_id) => {
-  const data = await ExperienceType.findAll({ where: { profession_id } });
-  return data;
-};
-
-const getAllExperienceTypes = asyncWrapper(async (_req, res) => {
-  const skList = await ExperienceType.findAll();
-  res.success(skList);
-});
-
-const createExperienceType = asyncWrapper(async (req, res) => {
-  const { name, profession_id } = req.body;
-  if (!name)
-    return res.fail("Experience type name is not provided", BAD_REQUEST);
-  if (!profession_id)
-    return res.fail(
-      "Every Experience type belongs to one profession",
-      BAD_REQUEST
-    );
-  const searchClause = {
-    name: {
-      [Op.iLike]: `%${name.toLowerCase().trim()}%`,
-    },
-  };
-  const found = await ExperienceType.findOne({ where: searchClause });
-  if (found) return res.success(found);
-  const model = {
-    name,
-    profession_id,
-  };
-  const sk = await ExperienceType.create(model);
-  if (!sk) return res.fail("Experience Type could not be created");
-  res.success(sk);
-});
-
-const getSingleExperienceType = asyncWrapper(async (req, res) => {
-  const { id } = req.params;
-  if (!id) return res.fail("ID is not provided", BAD_REQUEST);
-  const sk = await ExperienceType.findByPk(id);
-  res.success(sk);
-});
-
-const deleteExperienceType = asyncWrapper(async (req, res) => {
-  const { id } = req.params;
-  if (!id) res.fail("Invalid experience type ID", BAD_REQUEST);
-  await ExperienceType.destroy({
-    where: { id },
-  });
-  res.success("Experience Type Deleted");
-});
-
 module.exports = {
   _getSkillsOfProfession,
   getAllSkills,
   createSkill,
   getSingleSkill,
   deleteSkill,
-  _getExperienceTypesOfProfession,
-  getAllExperienceTypes,
-  createExperienceType,
-  getSingleExperienceType,
-  deleteExperienceType,
 };
--- routes/skill.js
+++ routes/skill.js
@@ -3,17 +3,26 @@ const {
   getAllSkills,
   createSkill,
   getSingleSkill,
+  deleteSkill,
+} = require("../controllers/skill");
+
+const {
   getAllExperienceTypes,
   createExperienceType,
   getSingleExperienceType,
   deleteExperienceType,
-  deleteSkill
-} = require("../controllers/skill");
+} = require("../controllers/experienceTypes");
 
 const router = Router();
 
-router.route("/experience-type/:id").get(getSingleExperienceType).delete(deleteExperienceType);
-router.route("/experience-type/").get(getAllExperienceTypes).post(createExperienceType);
+router
+  .route("/experience-type/:id")
+  .get(getSingleExperienceType)
+  .delete(deleteExperienceType);
+router
+  .route("/experience-type/")
+  .get(getAllExperienceTypes)
+  .post(createExperienceType);
 
 router.route("/:id").get(getSingleSkill).delete(deleteSkill);
 router.route("/").get(getAllSkills).post(createSkill);
